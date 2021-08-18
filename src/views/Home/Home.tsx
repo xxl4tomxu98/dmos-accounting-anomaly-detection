8309aed4aec7a4104e8fa965d119a4f06712e0ea
Jessamarie <jessamarie@users.noreply.github.com>
2021-08-18 11:12:12 -0400

    DMOS-159 fix issues with merge

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,13 +2,15 @@ import { Box, Heading, Image, Text } from '@chakra-ui/react';
 
 export function Home(): JSX.Element {
   return (
-    <Box>
-      <Heading as='h1' textAlign='center' lineHeight='normal' size='3xl'>
-        Simple Insights
-        <br />
-        <Text as='span' color='purple.500'>
-          Bold Predictions
-        </Text>
+    <Box w='50%' m='auto'>
+      <Heading as='h3' textAlign='center' lineHeight='normal' size='2xl'>
+        <Image
+          d='inline'
+          boxSize='100'
+          src='/home/home-1.jpg'
+          alt='home 1 logo'
+        />
+        Elite Forensic Accounting Agency
       </Heading>
 
       <Text d='block' as='i' mt='3' textAlign='center'>
