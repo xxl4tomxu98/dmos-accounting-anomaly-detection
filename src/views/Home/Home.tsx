93e71eed17a8aa863c69148849ed3a23cf6ec2ea
Jessamarie <jessamarie@users.noreply.github.com>
2021-08-17 21:29:50 -0400

    DMOS-159 Added main content to the home page; added home icons to public folder

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,18 +1,100 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Image, Text } from '@chakra-ui/react';
 import { useKeycloak } from '@react-keycloak/web';
 import { Redirect } from 'react-router-dom';
 
 export function Home(): JSX.Element {
   const { keycloak } = useKeycloak();
   return !keycloak.authenticated ? (
-    <Box>
-      <Heading as='h1' textAlign='center' lineHeight='normal' size='3xl'>
-        Simple Insights
-        <br />
-        <Text as='span' color='purple.500'>
-          Bold Predictions
-        </Text>
+    <Box w='50%' m='auto'>
+      <Heading as='h3' textAlign='center' lineHeight='normal' size='2xl'>
+        <Image
+          d='inline'
+          boxSize='100'
+          src='/home/home-1.jpg'
+          alt='home 1 logo'
+        />
+        Elite Forensic Accounting Agency
       </Heading>
+
+      <Text d='block' as='i' mt='3' textAlign='center'>
+        "Working to solve the root cause"
+      </Text>
+
+      <Text mt='10' mb='5'>
+        Providing:
+      </Text>
+
+      <Text mb='5'>
+        Accounting analysis to look beyond the numbers and deal with the
+        business reality of a situation.
+      </Text>
+
+      <Text mb='5'>
+        We collect, analyze, and evaluate evidential matters, interpret and
+        communicate findings to our clients
+      </Text>
+
+      <Text> Our Solutions Provide our Clients with:</Text>
+
+      <Text>
+        <Image
+          d='inline'
+          mr='2'
+          boxSize='75'
+          src='/home/home-2.png'
+          alt='home 2 logo'
+        />
+        <Text as='b'>Productivity and Business Value: </Text>
+        High quality, secure and working solutions
+      </Text>
+
+      <Text>
+        <Image
+          d='inline'
+          mr='2'
+          boxSize='75'
+          src='/home/home-3.png'
+          alt='home 3 logo'
+        />
+        <Text as='b'>Collaboration: </Text> Willing and able to coordinate on
+        every aspect of your needs
+      </Text>
+
+      <Text>
+        <Image
+          d='inline'
+          mr='2'
+          boxSize='75'
+          src='/home/home-4.png'
+          alt='home 4 logo'
+        />
+        <Text as='b'>Process Discipline: </Text> Processes and practices provide
+        consistent and efficient results
+      </Text>
+
+      <Text>
+        <Image
+          d='inline'
+          mr='2'
+          boxSize='75'
+          src='/home/home-5.png'
+          alt='home 5 logo'
+        />
+        <Text as='b'>Innovation: </Text>Innovative approach and creative
+        solutions result in high quality, secure and working solutions
+      </Text>
+
+      <Text>
+        <Image
+          d='inline'
+          mr='2'
+          boxSize='75'
+          src='/home/home-6.png'
+          alt='home 6 logo'
+        />
+        <Text as='b'>AI\ML: </Text>Technology to rapidly solve large data
+        problems and create business efficiency
+      </Text>
     </Box>
   ) : (
     <Redirect
