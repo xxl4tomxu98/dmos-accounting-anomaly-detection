7a8e34d2d790ad7e9f3cb628d7131bc13e048fe5
Jessamarie <jessamarie@users.noreply.github.com>
2021-08-18 20:11:24 -0400

    DMOS-159 adjust newline text to the right of images

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Heading, HStack, Image, Text } from '@chakra-ui/react';
 import { BaseRouteOnly } from 'src/components/BaseRouteOnly';
 export function Home(): JSX.Element {
   return (
@@ -34,65 +34,45 @@ export function Home(): JSX.Element {
 
         <Text fontSize='20'> Our Solutions Provide our Clients with:</Text>
 
-        <Text fontSize='16'>
-          <Image
-            d='inline'
-            mr='2'
-            boxSize='75'
-            src='/home/home-2.png'
-            alt='home 2 logo'
-          />
-          <Text as='b'>Productivity and Business Value: </Text>
-          High quality, secure and working solutions
-        </Text>
+        <HStack>
+          <Image mr='2' boxSize='75' src='/home/home-2.png' alt='home 2 logo' />
+          <Text fontSize='16'>
+            <Text as='b'>Productivity and Business Value: </Text>
+            High quality, secure and working solutions
+          </Text>
+        </HStack>
 
-        <Text fontSize='16'>
-          <Image
-            d='inline'
-            mr='2'
-            boxSize='75'
-            src='/home/home-3.png'
-            alt='home 3 logo'
-          />
-          <Text as='b'>Collaboration: </Text> Willing and able to coordinate on
-          every aspect of your needs
-        </Text>
+        <HStack>
+          <Image mr='2' boxSize='75' src='/home/home-3.png' alt='home 3 logo' />
+          <Text fontSize='16'>
+            <Text as='b'>Collaboration: </Text> Willing and able to coordinate
+            on every aspect of your needs
+          </Text>
+        </HStack>
 
-        <Text fontSize='16'>
-          <Image
-            d='inline'
-            mr='2'
-            boxSize='75'
-            src='/home/home-4.png'
-            alt='home 4 logo'
-          />
-          <Text as='b'>Process Discipline: </Text> Processes and practices
-          provide consistent and efficient results
-        </Text>
+        <HStack>
+          <Image mr='2' boxSize='75' src='/home/home-4.png' alt='home 4 logo' />
+          <Text fontSize='16'>
+            <Text as='b'>Process Discipline: </Text> Processes and practices
+            provide consistent and efficient results
+          </Text>
+        </HStack>
 
-        <Text fontSize='16'>
-          <Image
-            d='inline'
-            mr='2'
-            boxSize='75'
-            src='/home/home-5.png'
-            alt='home 5 logo'
-          />
-          <Text as='b'>Innovation: </Text>Innovative approach and creative
-          solutions result in high quality, secure and working solutions
-        </Text>
+        <HStack>
+          <Image mr='2' boxSize='75' src='/home/home-5.png' alt='home 5 logo' />
+          <Text fontSize='16'>
+            <Text as='b'>Innovation: </Text>Innovative approach and creative
+            solutions result in high quality, secure and working solutions
+          </Text>
+        </HStack>
 
-        <Text fontSize='16'>
-          <Image
-            d='inline'
-            mr='2'
-            boxSize='75'
-            src='/home/home-6.png'
-            alt='home 6 logo'
-          />
-          <Text as='b'>AI\ML: </Text>Technology to rapidly solve large data
-          problems and create business efficiency
-        </Text>
+        <HStack>
+          <Image mr='2' boxSize='75' src='/home/home-6.png' alt='home 6 logo' />
+          <Text fontSize='16'>
+            <Text as='b'>AI\ML: </Text>Technology to rapidly solve large data
+            problems and create business efficiency
+          </Text>
+        </HStack>
       </Box>
     </BaseRouteOnly>
   );
